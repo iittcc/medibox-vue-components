d12c7006632ee8316bae44ade7ec63f354f75a8d
Torben Jakobsen <kontakt@itechcare.dk>
2025-07-08 19:30:20 +0200

    chore: add ToastService and Toast component to WHO-5 application
    - Import ToastService from PrimeVue and register it in the app
    - Include Toast component in the WHO-5 template for notifications
    - Disable ESLint in components.d.ts for compatibility

--- src/who-5.ts
+++ src/who-5.ts
@@ -4,6 +4,7 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import './assets/sky.css';
 import App from './Who-5.vue';
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
 
 const app = createApp(App);
 
@@ -18,4 +19,6 @@ app.use(PrimeVue, {
         dateFormat : "dd/mm/yy" 
     }
 });
+
+app.use(ToastService)
 app.mount('#app');
